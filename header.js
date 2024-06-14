5a43e7efea2f3f4be3a0df2d4d1e829ee321df70
unknown <pmpnicolas@gmail.com>
2024-06-14 12:59:14 +0200

    second presentation

--- header.js
+++ header.js
@@ -1,26 +1,32 @@
-let appHeader = `
-<div class="up">
-    <div class="name">
-        <p><b>CLINICA DENTAL ORTEGO</b><br>C/ Madrazo 141. pral 1ª - Sarrià Sant-Gervasi<br>+34 646 272 974 | 93 209 71 67 | Dentalortego@gmail.com</p>
+document.addEventListener('DOMContentLoaded', function() {
+    let appHeader = `
+    <div class="up">
+        <div class="left">
+            <p><b>CLINICA DENTAL ORTEGO</b><br>C/ Madrazo 141. pral 1ª - Sarrià Sant-Gervasi<br>+34 646 272 974 | 93 209 71 67<br>Dentalortego@gmail.com</p>
+        </div>
+        <div class="center">
+            <img src="./images/logo.png" alt="Logo">
+            <img src="./images/slogan.jpg" class="placeholder-slogan">
+        </div>
+        <div class="right">
+            <div class="languages">
+                <div id="google_translate_element"></div>
+            </div>
+            <div class="social-links">
+                <a href="link_to_facebook" target="_blank"><img src="images/facebook_icon.png" alt="Facebook" style="width: 55px; height: 55px;"></a>
+                <a href="link_to_instagram" target="_blank"><img src="images/instagram_icon.png" alt="Instagram"></a>
+                <a href="link_to_facebook" target="_blank"><img src="images/whatsapp_icon.png" alt="Facebook";></a>
+            </div>
+        </div>
     </div>
-    <div class="logo">
-        <img src="./images/logo.png" alt="Logo">
-        <img src="./images/slogan.jpg" class="placeholder-slogan">
+
+    <div class="container purple topBotomBordersIn">
+        <a href="index.html">INICIO</a>
+        <a href="equipo.html">EQUIPO</a>
+        <a href="instalaciones.html">INSTALACIONES</a>
+        <a href="tratamientos.html">TRATAMIENTOS</a>
+        <a href="contacto.html">CONTACTO</a>
     </div>
-    <div class="social-links">
-        <a href="link_to_facebook" target="_blank"><img src="images/facebook_icon.png" alt="Facebook" style="width: 55px; height: 55px;"></a>
-        <a href="link_to_instagram" target="_blank"><img src="images/instagram_icon.png" alt="Instagram"></a>
-        <a href="link_to_facebook" target="_blank"><img src="images/whatsapp_icon.png" alt="Facebook";></a>
-    </div>
-</div>
-<nav>
-    <ul class="navigation">
-        <li><a href="index.html">INICIO</a></li>
-        <li><a href="equipo.html">EQUIPO</a></li>
-        <li><a href="instalaciones.html">INSTALACIONES</a></li>
-        <li><a href="tratamientos.html">TRATAMIENTOS</a></li>
-        <li><a href="contacto.html">CONTACTO</a></li>
-    </ul>
-</nav>
-`;
-document.getElementById("app-header").innerHTML = appHeader;
+    `;
+    document.getElementById("app-header").innerHTML = appHeader;
+});
