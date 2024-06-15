81600e78277463dee80bc4c8ced2de87f73cdc86
unknown <pmpnicolas@gmail.com>
2024-06-15 21:57:32 +0200

    final changes, prob only style from now on

--- header.js
+++ header.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
             <p>C/ Madrazo 141. pral 1ª<br>08021, Barcelona<br>+93 209 71 67 / 34 646 272 974<br>Dentalortego@gmail.com</p>
         </div>
         <div class="center">
-            <img style="width: 150px; margin: 0px 140px;"src="./images/logo.png" alt="Logo">
+            <img style="width: 150px; height: 116; margin: 0px 140px;"src="./images/logo2.png" alt="Logo">
             <p style="font-size: 29px;"><span style="font-size: 35px;">C</span>LíNICA <span style="font-size: 35px;">D</span>ENTAL <span style="font-size: 35px;">O</span>RTEGO</p>
         </div>
         <div class="right">
@@ -14,8 +14,8 @@ document.addEventListener('DOMContentLoaded', function() {
             </div>
             <div class="social-links">
                 <a href="link_to_facebook" target="_blank"><img src="images/facebook_icon.png" alt="Facebook" style="width: 45px; height: 45px;"></a>
-                <a href="link_to_instagram" target="_blank"><img src="images/instagram_icon.png" alt="Instagram";></a>
-                <a href="link_to_facebook" target="_blank"><img src="images/whatsapp_icon.png" alt="Facebook";></a>
+                <a href="link_to_instagram" target="_blank"><img src="images/instagram_icon.png" alt="Instagram" style="width: 55px; height: 55px"></a>
+                <a href="link_to_whatsapp" target="_blank"><img src="images/whatsapp_icon.png" alt="Facebook" style="width: 50px; height: 50px;"></a>
             </div>
         </div>
     </div>
