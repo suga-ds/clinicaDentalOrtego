000af225448b9c754213e8666fa705e45e5d34d1
suga-ds <pmpnicolas@gmail.com>
2024-10-18 21:01:39 +0200

    to update

--- header.js
+++ header.js
@@ -4,20 +4,20 @@ document.addEventListener('DOMContentLoaded', function () {
         <div class="left">
             <p>C/ Madrazo 141. pral 1ª</p>
             <p>08021, Barcelona</p>
-            <p>+93 209 71 67 / 34 646 272 974</p>
+            <p>+93 209 71 67 / +34 646 272 974</p>
             <p>dentalortego@gmail.com</p>
         </div>
         <div class="center">
-            <img style="width: 150px; height: 116px; margin: 0 140px;" src="./images/logo2.png" alt="Logo">
-            <p style="font-size: 29px;">
-                <span style="font-size: 35px;">C</span>L<span style="font-size: 29px;">Í</span>NICA 
-                <span style="font-size: 35px;">D</span>ENTAL <span style="font-size: 35px;">O</span>RTEGO
+            <img style="width: 200px; margin: 0 140px;" src="./images/logo2.png" alt="Logo">
+            <p style="font-size: 35px;">
+                <span style="font-size: 45px;">C</span>L<span style="font-size: 29px;">Í</span>NICA 
+                <span style="font-size: 45px;">D</span>ENTAL <span style="font-size: 45px;">O</span>RTEGO
             </p>
         </div>
         <div class="right">
-            <a href="./es/index.html">ES</a>
-            <a href="./en/index.html">EN</a>
-            <a href="./ca/index.html">CA</a>
+            <a href="./es/index.html">ESP</a>
+            <a href="./ca/index.html">CAT</a>
+            <a href="./en/index.html">ENG</a>
         </div>
     </div>
 
@@ -30,15 +30,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     <!-- Floating Buttons -->
     <div class="floating-icons">
-        <a href="mailto:dentalortego@gmail.com" class="floating-button email">
-            ✉ Escríbenos
+            <a href="tel:+34932097167" class="floating-button phone">
+            📞
         </a>
         <a href="https://wa.me/34646272974" target="_blank" class="floating-button whatsapp">
-            💬 WhatsApp
+            <img src="./images/whatsapp_icon.png" style="height: 35px;"></img>
         </a>
-        <a href="tel:+34932097167" class="floating-button phone">
-            📞 Llámanos
+        <a href="mailto:dentalortego@gmail.com" class="floating-button email">
+            ✉
         </a>
+
     </div>
     `;
 
