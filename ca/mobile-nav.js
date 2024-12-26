d494256400b45f74e890cb72d280351bfe90f970
unknown <pmpnicolas@gmail.com>
2024-12-26 02:17:48 +0100

    lang more changes 2

---
+++ ca/mobile-nav.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const spanishMobileNav = `
+        <div id="image-button-mobile">
+            <button aria-expanded="false" role="button" class="dropdown-button">MENÚ</button>
+            <img src="../images/logo-black.png" alt="logo" height="80px">
+        </div>
+        <nav id="mobile-navigation">
+            <ul>
+                <li><a href="/ca/index.html">INICI</a></li>
+                <li><a href="/ca/equip.html">EQUIP</a></li>
+                <li><a href="/ca/tractaments.html">TRACTAMENTS</a></li>
+                <li><a href="/ca/instalacions.html">INSTALACIONS</a></li>
+                <li><a href="/ca/contacte.html">CONTACTE</a></li>
+            </ul>
+        </nav>
+    `;
+
+    document.querySelector('.navigation-mobile').innerHTML = spanishMobileNav;
+});
---
+++ ca/nav.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let spanishNav = `
+        <img src="../images/logo-white.png" alt="logo" height=50px>
+        <nav class="menu">
+            <a href="/ca/index.html">INICI</a>
+            <a href="/ca/equip.html">EQUIP</a>
+            <a href="/ca/tractaments.html">TRACTAMENTS</a>
+            <a href="/ca/instalacions.html">INSTALACIONS</a>
+            <a href="/ca/contacte.html">CONTACTE</a>
+        </nav>
+    `;
+
+    document.querySelector(".sidebar").innerHTML = spanishNav;
+});
